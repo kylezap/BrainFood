c2cbe126b3a3dde29874584b0499138913e52487
Bryan O <bocampo91@yahoo.com>
2024-04-02 14:36:22 -0400

    Uploading code for random facts

---
+++ facts.js
@@ -0,0 +1,29 @@
+let requestURL = `https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true`;
+const factButton = document.getElementsByClassName('fact-button');
+
+const options = {
+    headers: {
+        'X-RapidAPI-Key': '8bb08a213emshbf4c57b8848c8bap1878e7jsn1d50ea401238',
+        'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+    }
+};
+/*
+function generateFacts() {
+    fetch(requestURL, options).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        console.log(data.text);
+
+    })
+}*/
+
+factButton.addEventListener('click', function generateFacts(event) {
+    fetch(requestURL, options).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        console.log(data.text);
+
+    })
+})
