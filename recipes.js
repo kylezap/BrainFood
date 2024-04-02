2cef2d71ee42f978b96883783095e20171b9d45f
kylezap <kzapcic+github@gmail.com>
2024-04-02 14:34:12 -0400

    add Yummly api call

--- recipes.js
+++ recipes.js
@@ -1,21 +1,41 @@
-const foo = document.getElementById()
-const foo = document.getElementById()
-const foo = document.getElementById()
-const foo = document.getElementById()
+const formSubmit = $('#search');
+const input = $('#search-input')
+// const foo = document.getElementById()
+// const foo = document.getElementById()
+// const foo = document.getElementById()
 
-const url = 'https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7d8c042289mshc514aced7d88fe4p1a19f8jsn3772f5b49341',
-		'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
-	}
+function apiCall(event) {
+	event.preventDefault();
+	// const formSubmitInput = formSubmit.val();
+	;
+	console.log("Inside API Call");
+	const url = `https://yummly2.p.rapidapi.com/feeds/search?start=0&maxResult=10&q=${input.val()}`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '7d8c042289mshc514aced7d88fe4p1a19f8jsn3772f5b49341',
+			'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+		}
+	};
+
+	fetch(url, options)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+		});
+
+	
+
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+
+formSubmit.on('submit', apiCall);
+
+
+//feed[i].content.details.images
+//feed[i].content.details.name:
+//feed[i].content.details.numberOfServings:
+//feed[i].content.details.rating:
+//feed[i].content.details.totalTime: 
